f11d3a0f231adb3cbf331ac61007ed0204882199
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2022-05-18 17:26:17 -0600

    Add React router

--- mick-portfolio/src/App.js
+++ mick-portfolio/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import Header from './Components/header'
+import Footer from './Components/footer'
+import Home from './Pages/home'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi!</h1>
+      <Header />
+      <div>
+        <Routes>
+          <Route exact path="/" component={Home} />
+          {/* <Route exact path="/" component={routerProps => <Home routerProps={routerProps} />} /> */}
+        </Routes>
+      </div>
+      <Footer />
     </div>
   );
 }
