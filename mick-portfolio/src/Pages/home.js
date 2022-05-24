cd482a83ab075f135f6bb66eedbf808bc6c3caa0
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2022-05-23 21:31:36 -0600

    Add about page

--- mick-portfolio/src/Pages/home.js
+++ mick-portfolio/src/Pages/home.js
@@ -1,15 +1,25 @@
-import Header from "../Components/Header"
-import Footer from "../Components/Footer"
-// import { Layout } from '../Components/layout'
 
 const Home = () => {
     return(
         <>
-        {/* <Header /> */}
         <div>
-            <h1>Hey Man!</h1>
+            <h1>About:</h1>
+            <p>I’m a motivated front end developer with back end experience 
+                who recently made a career change from working in live entertainment 
+                to software engineering. My love for live music led me to chase a 
+                career in ticketing and I landed a position at AXS who manages the 
+                ticketing for over 10 venues (including Red Rocks) in the Denver area. 
+                My experience in ticketing is what led me to software engineering.  
+                At AXS, I became a go-to person to control event pages, test QA functionality, 
+                and manage event builds. Through this position, I realized my passion 
+                for this work and decided to pursue a career in software engineering.</p>
+            <p>Over the past few years I have been expanding my knowledge of programming 
+                and recently completed Flatiron Software Engineer Bootcamp. The more I 
+                learn about technologies such as React, JavaScript and Ruby on Rails, 
+                the more intrigued I become. I'm excited to apply the skills acquired 
+                at Flatiron and AXS to a position in this ever-evolving industry.</p>
+            <p></p>
         </div>
-        {/* <Footer /> */}
         </>
     )
 }
