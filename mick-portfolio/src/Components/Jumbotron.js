9c4e1546674b131c9aa5b2c31cefbda137e8ffd8
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2022-06-07 10:28:21 -0600

    Update Header and add facebook and linkedin links

--- mick-portfolio/src/Components/Jumbotron.js
+++ mick-portfolio/src/Components/Jumbotron.js
@@ -1,4 +1,4 @@
-import { Container } from 'react-bootstrap'
+// import { Container } from 'react-bootstrap'
 import styled from 'styled-components'
 import StarSky from '../Assets/StarSky.jpg'
 
--- mick-portfolio/src/Components/header.js
+++ mick-portfolio/src/Components/header.js
@@ -7,29 +7,53 @@ const Styles = styled.div`
     }
 
     .navbar-brand, .navbar-nav .nav-link {
-       color: #bbb
+       color: #bbb;
+       margin-left: 5px
     }
 
     .navbar-brand, .navbar-nav .nav-link:hover {
         color: white  
      }
 
-     .navbar-toggler-icon {
-         color: white
-     }
+    .navbar-toggler-icon {
+        color: white
+    }
+
+    .fa {
+        padding: 15px;
+        font-size: 20px;
+        width: 50px;
+        text-align: center;
+        text-decoration: none;
+        border-radius: 50%;
+        margin-bottom: 10px;
+        margin-top: 10px;
+        margin-left: 10px
+    }
+
+    .fa-twitter {
+        background: #55ACEE;
+        color: white;
+    }
+
+    .fa-linkedin {
+        background: #0077b5;
+        color: white;
+    }
 `;
 
 const Header = () => {
     return(
         <Styles>
             <Navbar expand='1g'>
-                <Navbar.Brand href="/">Shya! Roight!</Navbar.Brand>
+                <Navbar.Brand href="/">Mick Nelson </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"  />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
                         <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
                         <Nav.Item><Nav.Link href="/projects">Projects</Nav.Link></Nav.Item>
-                        <Nav.Item><Nav.Link href="">Boingo</Nav.Link></Nav.Item>
+                        <a href="https://www.linkedin.com/in/mick-nelson-denver/" className="fa fa-linkedin" target="_blank"></a>
+                        <a href="" className="fa fa-twitter"></a>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
